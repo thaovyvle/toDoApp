843aa8d03ba96180b2b29dba70929938c6b5b6ad
thaovyvle <122627050+thaovyvle@users.noreply.github.com>
2024-02-22 20:34:33 -0500

    Deploy

--- node_modules/traverse/index.js
+++ node_modules/traverse/index.js
@@ -30,25 +30,6 @@ var objectKeys = Object.keys || function keys(obj) {
 	return res;
 };
 
-var propertyIsEnumerable = Object.prototype.propertyIsEnumerable;
-var getOwnPropertySymbols = Object.getOwnPropertySymbols; // eslint-disable-line id-length
-
-// TODO: use reflect.ownkeys and filter out non-enumerables
-function ownEnumerableKeys(obj) {
-	var res = objectKeys(obj);
-
-	// Include enumerable symbol properties.
-	if (getOwnPropertySymbols) {
-		var symbols = getOwnPropertySymbols(obj);
-		for (var i = 0; i < symbols.length; i++) {
-			if (propertyIsEnumerable.call(obj, symbols[i])) {
-				res.push(symbols[i]);
-			}
-		}
-	}
-	return res;
-}
-
 // TODO: use object.hasown
 var hasOwnProperty = Object.prototype.hasOwnProperty || function (obj, key) {
 	return key in obj;
@@ -74,14 +55,14 @@ function copy(src) {
 			dst = {};
 		} else {
 			var proto = (src.constructor && src.constructor.prototype)
-				|| src.__proto__
-				|| {};
+                || src.__proto__
+                || {};
 			var T = function T() {}; // eslint-disable-line func-style, func-name-matching
 			T.prototype = proto;
 			dst = new T();
 		}
 
-		forEach(ownEnumerableKeys(src), function (key) {
+		forEach(objectKeys(src), function (key) {
 			dst[key] = src[key];
 		});
 		return dst;
@@ -143,7 +124,7 @@ function walk(root, cb, immutable) {
 		function updateState() {
 			if (typeof state.node === 'object' && state.node !== null) {
 				if (!state.keys || state.node_ !== state.node) {
-					state.keys = ownEnumerableKeys(state.node);
+					state.keys = objectKeys(state.node);
 				}
 
 				state.isLeaf = state.keys.length === 0;
@@ -300,7 +281,7 @@ Traverse.prototype.clone = function () {
 			parents.push(src);
 			nodes.push(dst);
 
-			forEach(ownEnumerableKeys(src), function (key) {
+			forEach(objectKeys(src), function (key) {
 				dst[key] = clone(src[key]);
 			});
 
@@ -319,7 +300,7 @@ function traverse(obj) {
 }
 
 // TODO: replace with object.assign?
-forEach(ownEnumerableKeys(Traverse.prototype), function (key) {
+forEach(objectKeys(Traverse.prototype), function (key) {
 	traverse[key] = function (obj) {
 		var args = [].slice.call(arguments, 1);
 		var t = new Traverse(obj);
